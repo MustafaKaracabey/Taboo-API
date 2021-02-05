a7bcd8c75ea870aa4df92ed381984bfa0b6bb5f6
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-02-05 08:09:54 -0800

    I Added Data Layer Fundamental Featues (fixed)

--- Taboo/Taboo.Core/Models/Word.cs
+++ Taboo/Taboo.Core/Models/Word.cs
@@ -8,7 +8,7 @@ namespace Taboo.Core.Models
     {
         public int WordId { get; set; }
         public string WordName { get; set; }
-        public bool DifficultyLevel { get; set; } // 1 -->Hard  0 -->Kolay
+        public bool DifficultyLevel { get; set; } // Logic : 1 -->Hard  0 -->Kolay
 
         public Word()
         {
