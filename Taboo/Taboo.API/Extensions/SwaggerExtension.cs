e699777ca44287358fefd56ba07ce1fae704c394
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-02-05 05:28:48 -0800

    I Added Swagger Open API

---
+++ Taboo/Taboo.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taboo.API.Extensions
+{
+    public static  class SwaggerExtension
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1.0",
+                    Title = "Taboo Web API",
+                    Description = "A Simple Taboo Game  ASP.NET Core Web API",
+                    TermsOfService = new System.Uri("https://www.linkedin.com/in/mustafa-karacabey-2046741a3/"),
+                    Contact = new OpenApiContact() { Name = "Mustafa Karacabey", Email = "mustafa.karacabey@outlook.com" }
+                });
+
+            });
+        }
+
+
+        public static void UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Taboo Web API");
+
+            });
+        }
+    }
+}
--- Taboo/Taboo.API/Startup.cs
+++ Taboo/Taboo.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Taboo.API.Extensions;
 
 namespace Taboo.API
 {
@@ -25,6 +26,9 @@ namespace Taboo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Adding Swagger Service For Docs
+            services.AddSwagger();
+
             services.AddControllers();
         }
 
@@ -41,6 +45,9 @@ namespace Taboo.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //Add Swagger MiddleWare
+            app.UseCustomSwagger();
 
             app.UseEndpoints(endpoints =>
             {
